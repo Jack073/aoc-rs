9ba4c482868f909356525fb9f61a3683248d47a8
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-08 22:41:09 +0000

    add 2019 day 1 (2)

--- 2019/01/src/main.rs
+++ 2019/01/src/main.rs
@@ -12,6 +12,25 @@ fn part_one() -> usize {
         .sum()
 }
 
+fn recursive_fuel_calculation(fuel: usize) -> usize {
+    if fuel < 6 {
+        0
+    } else {
+        let cost = (fuel / 3) - 2;
+        cost + recursive_fuel_calculation(cost)
+    }
+}
+
+fn part_two() -> usize {
+    BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .filter_map(|l| match l {
+            Ok(l) => Some(l.parse::<usize>().unwrap_or(0)),
+            _ => None,
+        })
+        .map(recursive_fuel_calculation)
+        .sum()
+}
 fn main() {
-    println!("{} ", part_one());
+    println!("{} ", part_two());
 }
