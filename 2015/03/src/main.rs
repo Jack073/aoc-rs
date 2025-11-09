c09c742ac28942f108ba97ef67e1fe6cca2385eb
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-09 17:36:44 +0000

    add 2015 day 3 (2)

--- 2015/03/src/main.rs
+++ 2015/03/src/main.rs
@@ -38,6 +38,65 @@ fn part_one() -> usize {
         .count()
 }
 
+fn part_two() -> usize {
+    let mut s = String::new();
+
+    File::open("input.txt")
+        .expect("input file not found")
+        .read_to_string(&mut s)
+        .expect("read error");
+
+    let mut coords = s
+        .chars()
+        .enumerate()
+        .filter_map(|(i, c)| if i % 2 == 0 { Some(c) } else { None })
+        .map(|c| match c {
+            '^' => (0, 1),
+            'v' => (0, -1),
+            '>' => (-1, 0),
+            '<' => (1, 0),
+            _ => panic!("bad character"),
+        })
+        .scan((0, 0), |acc, coord| {
+            acc.0 += coord.0;
+            acc.1 += coord.1;
+            Some(*acc)
+        })
+        .collect::<Vec<_>>();
+
+    let robo_coords = s
+        .chars()
+        .enumerate()
+        .filter_map(|(i, c)| if i % 2 == 1 { Some(c) } else { None })
+        .map(|c| match c {
+            '^' => (0, 1),
+            'v' => (0, -1),
+            '>' => (-1, 0),
+            '<' => (1, 0),
+            _ => panic!("bad character"),
+        })
+        .scan((0, 0), |acc, coord| {
+            acc.0 += coord.0;
+            acc.1 += coord.1;
+            Some(*acc)
+        })
+        .collect::<Vec<_>>();
+
+    coords.extend(robo_coords);
+
+    coords.push((0, 0));
+    coords.push((0, 0));
+
+    coords.sort_by_key(|n| n.0.to_string() + ">" + &n.1.to_string());
+
+    coords
+        .iter()
+        .chunk_by(|&a| a)
+        .into_iter()
+        .filter_map(|n| if n.1.count() >= 1 { Some(1) } else { None })
+        .count()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
