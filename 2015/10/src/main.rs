799507e1b5ba2593add10b4d2b02e36655ae03c5
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-16 15:23:36 +0000

    add 2015 day 10 (1)

---
+++ 2015/10/src/main.rs
@@ -0,0 +1,35 @@
+use itertools::Itertools;
+
+fn look_and_say(inp: Vec<usize>, iterations: usize) -> Vec<usize> {
+    if iterations == 0 {
+        inp
+    } else {
+        look_and_say(
+            inp.into_iter()
+                .chunk_by(|&c| c)
+                .into_iter()
+                .map(|c| [c.1.count(), c.0])
+                .flatten()
+                .collect(),
+            iterations - 1,
+        )
+    }
+}
+
+fn part_one() {
+    println!(
+        "{}",
+        look_and_say(
+            "1113222113"
+                .chars()
+                .map(|n| (n as u16 as usize) - '0' as u16 as usize)
+                .collect::<Vec<_>>(),
+            40
+        )
+        .len()
+    );
+}
+
+fn main() {
+    part_one();
+}
