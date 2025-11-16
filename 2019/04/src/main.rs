91ef5730e4f51745afd6cbffc334e487d7a60dc7
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-16 15:44:55 +0000

    add 2019 day 4 (2)

--- 2019/04/src/main.rs
+++ 2019/04/src/main.rs
@@ -21,6 +21,27 @@ fn part_one() -> usize {
         .count()
 }
 
+fn part_two() -> usize {
+    (359282..820401)
+        .map(|n| {
+            [
+                n / 100000,
+                (n / 10000) % 10,
+                (n / 1000) % 10,
+                (n / 100) % 10,
+                (n / 10) % 10,
+                n % 10,
+            ]
+        })
+        .filter(|n| n.is_sorted())
+        .filter(|n| n
+            .iter()
+            .chunk_by(|&k| k)
+            .into_iter()
+            .any(|b| b.1.count() == 2))
+        .count()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
