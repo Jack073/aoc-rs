45f34f65cb28bc444cb0912b21637d6b6d4d5260
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-16 15:43:55 +0000

    add 2019 day 4 (1)

---
+++ 2019/04/src/main.rs
@@ -0,0 +1,26 @@
+use itertools::Itertools;
+
+fn part_one() -> usize {
+    (359282..820401)
+        .map(|n| {
+            [
+                n / 100000,
+                (n / 10000) % 10,
+                (n / 1000) % 10,
+                (n / 100) % 10,
+                (n / 10) % 10,
+                n % 10,
+            ]
+        })
+        .filter(|n| n.is_sorted())
+        .filter(|n| n
+            .iter()
+            .chunk_by(|&k| k)
+            .into_iter()
+            .any(|b| b.1.count() >= 2))
+        .count()
+}
+
+fn main() {
+    println!("{}", part_one());
+}
