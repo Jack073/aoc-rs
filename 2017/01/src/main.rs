f1f3c9f536826749f51457cc69b787b68b6e97dc
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-16 16:32:47 +0000

    add 2017 day 1 (2)

--- 2017/01/src/main.rs
+++ 2017/01/src/main.rs
@@ -22,6 +22,27 @@ fn part_one() -> usize {
         .sum()
 }
 
+fn part_two() -> usize {
+    let mut s = String::new();
+
+    File::open("input.txt")
+        .expect("input file error")
+        .read_to_string(&mut s)
+        .expect("read error");
+
+    let mut chars_one = s.chars().collect::<Vec<_>>();
+    let chars_two = chars_one.clone();
+
+    let halfway = chars_one.len() / 2;
+
+    chars_one = [&chars_two[halfway..], &chars_two[..halfway]].concat();
+
+    izip!(chars_one, chars_two)
+        .filter(|(a, b)| a == b)
+        .map(|(a, _)| a as usize - '0' as usize)
+        .sum()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
