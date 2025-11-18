f61544a64e57035f0f27b57b6372e4c401c84016
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-18 20:24:45 +0000

    add 2017 day 4 (2)

--- 2017/04/src/main.rs
+++ 2017/04/src/main.rs
@@ -21,6 +21,26 @@ fn part_one() -> usize {
         .count()
 }
 
+fn part_two() -> usize {
+    BufReader::new(File::open("input.txt").expect("input file error"))
+        .lines()
+        .filter_map(|l| match l {
+            Ok(l) => Some(l),
+            Err(_) => None,
+        })
+        .filter(|l| {
+            l.split_ascii_whitespace()
+                .map(|k| k.chars().sorted().collect::<String>())
+                .collect::<Vec<_>>()
+                .into_iter()
+                .sorted()
+                .chunk_by(|k| k.clone())
+                .into_iter()
+                .all(|g| g.1.count() == 1)
+        })
+        .count()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
