b0a556bf9eb7393470675807a015ba43191197ee
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-18 20:43:29 +0000

    add 2017 day 5 (2)

--- 2017/05/src/main.rs
+++ 2017/05/src/main.rs
@@ -25,6 +25,35 @@ fn part_one() -> usize {
     jumps
 }
 
+fn part_two() -> usize {
+    let mut instructions = BufReader::new(File::open("input.txt").expect("input file error"))
+        .lines()
+        .filter_map(|l| match l {
+            Ok(n) => Some(n),
+            Err(_) => None,
+        })
+        .map(|l| l.parse::<isize>().unwrap())
+        .collect::<Vec<_>>();
+    let mut jumps = 0;
+    let mut pos = 0;
+
+    while pos < instructions.len() {
+        jumps+=1;
+
+        let next_jump = &mut instructions[pos];
+
+        pos = pos.saturating_add_signed(*next_jump);
+
+        if *next_jump >= 3 {
+            *next_jump -= 1;
+        } else {
+            *next_jump += 1;
+        }
+    }
+
+    jumps
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
