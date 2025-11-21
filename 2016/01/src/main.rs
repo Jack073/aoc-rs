bfa8b89f6f949549e8d50bebadbd9afea87a998b
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-21 18:17:16 +0000

    add 2016 day 1 (2)

--- 2016/01/src/main.rs
+++ 2016/01/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::Read;
 
@@ -20,6 +21,13 @@ impl InstructionCounter {
 
     fn taxicab_distance(&self) -> usize {
         (self.north.abs_diff(self.south)) + (self.east.abs_diff(self.west))
+    }
+
+    fn effective_coords(&self) -> (isize, isize) {
+        (
+            (self.north as isize) - (self.south as isize),
+            (self.east as isize) - (self.west as isize),
+        )
     }
 }
 
@@ -91,6 +99,58 @@ fn part_one() -> usize {
     accumulator.taxicab_distance()
 }
 
+fn part_two() -> usize {
+    let mut s = String::new();
+
+    File::open("input.txt")
+        .expect("input file error")
+        .read_to_string(&mut s)
+        .expect("read error");
+
+    let mut accumulator = InstructionCounter {
+        north: 0,
+        south: 0,
+        east: 0,
+        west: 0,
+    };
+
+    let mut direction = Direction::North;
+
+    let mut set: HashSet<(isize, isize)> = HashSet::new();
+    set.insert((0, 0));
+
+    s.split(", ")
+        .map(|instruction| {
+            let mut chars = instruction.chars();
+
+            let direction = chars.next().unwrap();
+
+            (
+                direction,
+                chars.collect::<String>().parse::<usize>().unwrap(),
+            )
+        })
+        .filter(|(d, distance)| {
+            direction = match d {
+                'L' => direction.left(),
+                'R' => direction.right(),
+                _ => panic!("invalid direction"),
+            };
+
+            for _ in 0..*distance {
+                accumulator.increment_direction(&direction, 1);
+                if !set.insert(accumulator.effective_coords()) {
+                    return true;
+                }
+            }
+
+            false
+        })
+        .next();
+
+    accumulator.taxicab_distance()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
