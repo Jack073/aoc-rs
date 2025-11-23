84b1d24b4ece4d48f889c0b43f63558fdd8609c0
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-23 13:02:06 +0000

    add 2016 day 3 (2)

--- 2016/03/src/main.rs
+++ 2016/03/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -22,6 +23,41 @@ fn part_one() -> usize {
         .count()
 }
 
+fn part_two() -> usize {
+    BufReader::new(File::open("input.txt").expect("input file error"))
+        .lines()
+        .map(|l| l.unwrap())
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| {
+            let c = chunk
+                .map(|line| {
+                    line.split_ascii_whitespace()
+                        .map(|n| n.parse::<usize>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>();
+
+            vec![
+                [c[0][0], c[1][0], c[2][0]],
+                [c[0][1], c[1][1], c[2][1]],
+                [c[0][2], c[1][2], c[2][2]],
+            ]
+        })
+        .flatten()
+        .filter_map(|line| {
+            if [(0, 1, 2), (0, 2, 1), (1, 2, 0)]
+                .iter()
+                .all(|sides| line[sides.0] + line[sides.1] > line[sides.2])
+            {
+                Some(true)
+            } else {
+                None
+            }
+        })
+        .count()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
