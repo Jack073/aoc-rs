621c63c8ab99eef7018b2b6a0ab65dafb7b08842
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-23 17:31:04 +0000

    add 2016 day 6 (2)

--- 2016/06/src/main.rs
+++ 2016/06/src/main.rs
@@ -23,6 +23,30 @@ fn part_one() -> String {
         .collect::<String>()
 }
 
+
+fn part_two() -> String {
+    let mut frequencies = [[0; 26]; 8];
+
+    BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .into_iter()
+        .map(|l| l.unwrap())
+        .for_each(|line| {
+            line.chars().enumerate().into_iter().for_each(|(i, c)| {
+                let index = (c as usize) - ('a' as usize);
+
+                frequencies[i][index] += 1;
+            })
+        });
+
+    frequencies
+        .map(|c| (c.iter().enumerate().min_by_key(|l| l.1).unwrap().0) + 'a' as usize)
+        .map(|c| c as u8 as char)
+        .iter()
+        .collect::<String>()
+}
+
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
