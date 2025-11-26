b1e2f48ac7766cca5b06dd5004924c9f17c38cc3
Jack <40802798+Jack073@users.noreply.github.com>
2025-11-26 22:09:44 +0000

    add 2022 day 1 (2)

--- 2022/01/src/main.rs
+++ 2022/01/src/main.rs
@@ -15,6 +15,21 @@ fn part_one() -> usize {
         .unwrap()
 }
 
+fn part_two() -> usize {
+    let list = BufReader::new(File::open("input.txt").expect("input.txt error"))
+        .lines()
+        .map(|l| l.unwrap())
+        .chunk_by(|line| line.is_empty())
+        .into_iter()
+        .filter(|g| !g.0)
+        .map(|line| line.1.map(|c| c.parse::<usize>().unwrap()).sum::<usize>())
+        .sorted()
+        .rev()
+        .collect_vec();
+
+    list[0] + list[1] + list[2]
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
