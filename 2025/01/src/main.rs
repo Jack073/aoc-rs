e4c6c4f305bab92f366592227b0103771c309a36
Jack <40802798+Jack073@users.noreply.github.com>
2025-12-01 05:11:49 +0000

    add 2025 day 1 (1)

---
+++ 2025/01/src/main.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn part_one() -> usize {
+    let mut point = 50;
+    let mut hit_zero = 0;
+
+    for d in BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .map(|l| l.unwrap())
+        .map(|l| {
+            let sign = l.chars().nth(0).unwrap();
+            let num = l
+                .chars()
+                .skip(1)
+                .collect::<String>()
+                .parse::<isize>()
+                .unwrap();
+
+            if sign == 'L' { -num } else { num }
+        })
+    {
+        point = (point + d) % 100;
+        if point == 0 {
+            hit_zero += 1;
+        }
+    }
+
+    hit_zero
+}
+
+fn main() {
+    println!("{}", part_one());
+}
