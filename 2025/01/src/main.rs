707352cb44a2a2d0e7c2380089f2291c8a342f0d
Jack <40802798+Jack073@users.noreply.github.com>
2025-12-01 05:13:14 +0000

    add 2025 day 1 (2)

--- 2025/01/src/main.rs
+++ 2025/01/src/main.rs
@@ -29,6 +29,37 @@ fn part_one() -> usize {
     hit_zero
 }
 
+fn part_two() -> usize {
+    let mut point = 50;
+    let mut hit_zero = 0;
+
+    for d in BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .map(|l| l.unwrap())
+        .map(|l| {
+            let sign = l.chars().nth(0).unwrap();
+            let num = l
+                .chars()
+                .skip(1)
+                .collect::<String>()
+                .parse::<isize>()
+                .unwrap();
+
+            if sign == 'L' { -num } else { num }
+        })
+    {
+        let delta = d.signum();
+        for _ in (0..d.abs()) {
+            point = (point + delta) % 100;
+            if point == 0 {
+                hit_zero += 1;
+            }
+        }
+    }
+
+    hit_zero
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
