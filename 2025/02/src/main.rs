b2c6ab0e55f6f64c90086fba2eb96dc2b80db674
Jack <40802798+Jack073@users.noreply.github.com>
2025-12-02 10:40:44 +0000

    add 2025 day 2 (2)

--- 2025/02/src/main.rs
+++ 2025/02/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, Read};
+use std::io::Read;
 
 fn part_one() -> usize {
     let mut s = String::new();
@@ -35,6 +35,40 @@ fn part_one() -> usize {
         .sum()
 }
 
+fn part_two() -> usize {
+    let mut s = String::new();
+
+    File::open("input.txt")
+        .unwrap()
+        .read_to_string(&mut s)
+        .unwrap();
+
+    s.split(",")
+        .map(|range| {
+            let mut split_range = range
+                .split_terminator("-")
+                .map(str::parse::<usize>)
+                .map(Result::unwrap);
+            let start = split_range.next().unwrap();
+            let end = split_range.next().unwrap();
+
+            (start..=end)
+                .filter(|num| {
+                    let s = num.to_string();
+
+                    let chars = s.chars().collect::<Vec<char>>();
+                    let len = chars.len();
+                    let midpoint = len / 2;
+
+                    (1..=midpoint)
+                        .filter(|p| len % p == 0)
+                        .any(|rep_len| (0..(len - rep_len)).all(|c| chars[c] == chars[c + rep_len]))
+                })
+                .sum::<usize>()
+        })
+        .sum()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
