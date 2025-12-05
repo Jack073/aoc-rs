47b188340768274bb2460b4d8ca1e5167c3be6d0
Jack <40802798+Jack073@users.noreply.github.com>
2025-12-05 11:04:51 +0000

    add 2025 day 5 (2)

--- 2025/05/src/main.rs
+++ 2025/05/src/main.rs
@@ -1,9 +1,11 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::mem::swap;
 
 struct Range {
     start: usize,
     end: usize,
+    obsolete: bool,
 }
 
 impl Range {
@@ -18,6 +20,7 @@ impl FromIterator<usize> for Range {
         Self {
             start: it.next().unwrap(),
             end: it.last().unwrap(),
+            obsolete: false,
         }
     }
 }
@@ -43,6 +46,79 @@ fn part_one() -> usize {
         .count()
 }
 
+fn part_two() -> usize {
+    let mut reader = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .map(|n| n.unwrap());
+
+    let mut ranges = reader
+        .take_while(|line| line.len() > 0)
+        .map(|line| {
+            line.split_terminator('-')
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect::<Range>()
+        })
+        .collect::<Vec<_>>();
+
+    let len = ranges.len();
+
+    for mut r1 in 0..len {
+        if ranges[r1].obsolete {
+            continue;
+        }
+
+        for mut r2 in 0..len {
+            if r1 == r2 {
+                continue;
+            }
+
+            if ranges[r2].obsolete {
+                continue;
+            }
+
+            let mut obsolete = false;
+            let mut extend = false;
+            {
+                let range_1 = &ranges[r1];
+                let range_2 = &ranges[r2];
+
+                if range_1.start <= range_2.start && range_2.start <= range_1.end {
+                    if range_2.end > range_1.end {
+                        extend = true;
+                    } else {
+                        obsolete = true;
+                    }
+                } else {
+                    if range_2.start <= range_1.start && range_1.start <= range_2.end {
+                        if range_1.end > range_2.end {
+                            extend = true;
+                        } else {
+                            obsolete = true;
+                        }
+
+                        swap(&mut r1, &mut r2);
+                    }
+                }
+            }
+
+            if extend {
+                ranges[r1].end = ranges[r2].end;
+                ranges[r2].obsolete = true;
+            }
+
+            if obsolete {
+                ranges[r2].obsolete = true;
+            }
+        }
+    }
+
+    ranges
+        .iter()
+        .filter(|r| !r.obsolete)
+        .map(|r| 1 + r.end - r.start)
+        .sum()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
