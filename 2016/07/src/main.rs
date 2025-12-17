c7de944e73941d9d900e54fea54784b118d237d2
Jack <40802798+Jack073@users.noreply.github.com>
2025-12-17 23:46:38 +0000

    add 2016 day 7 (2)

--- 2016/07/src/main.rs
+++ 2016/07/src/main.rs
@@ -28,6 +28,45 @@ fn part_one() -> usize {
         .count()
 }
 
+
+fn aba_check(window: &(char, char, char)) -> bool {
+    window.0 == window.2 && window.0 != window.1
+}
+
+fn construct_aba_vec<'a, T>(iter: T) -> Vec<(char, char, char)>
+where
+    T: Iterator<Item = &'a &'a str>,
+{
+    iter.map(|&n| {
+        n.chars()
+            .tuple_windows()
+            .filter(aba_check)
+            .collect::<Vec<_>>()
+    })
+        .flatten()
+        .collect::<Vec<_>>()
+}
+
+fn part_two() -> usize {
+    BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .map(|l| l.unwrap())
+        .filter(|x| {
+            let split = x.rsplit(|c| c == '[' || c == ']').collect::<Vec<&str>>();
+
+            let supernet = split.iter().step_by(2);
+            let hypernet = split.iter().skip(1).step_by(2);
+
+            let super_matches = construct_aba_vec(supernet);
+            let hyper_matches = construct_aba_vec(hypernet);
+
+            super_matches
+                .iter()
+                .any(|sup| hyper_matches.iter().any(|m| sup.0 == m.1 && sup.1 == m.0))
+        })
+        .count()
+}
+
 fn main() {
     println!("{}", part_one());
 }
