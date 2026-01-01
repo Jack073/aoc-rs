affa58e1464986df1ddc5cde826a0913e2602660
Jack <40802798+Jack073@users.noreply.github.com>
2026-01-01 22:44:50 +0000

    add 2020 day 9 (2)

--- 2020/09/src/main.rs
+++ 2020/09/src/main.rs
@@ -57,6 +57,56 @@ fn part_one() -> usize {
     .0
 }
 
+fn part_two() -> usize {
+    let nums = BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .map(|l| l.unwrap().parse().unwrap())
+        .collect::<Vec<usize>>();
+
+    let target_value = StatefulIterator {
+        src: nums.iter().map(|c| *c),
+        buf: [0; 25],
+        pos: 0,
+        init: false,
+    }
+        .into_iter()
+        .find(|(_, possible)| !possible)
+        .unwrap()
+        .0;
+
+    let mut skip = 0;
+    loop {
+        let mut cum = 0;
+        let mut min = usize::MAX;
+        let mut max = usize::MIN;
+
+        // This would probably be better handled by implementing a new iterator type to cycle
+        // from a set starting point. But for this problem, this is slightly inefficient but works.
+        let _ = nums
+            .iter()
+            .skip(skip)
+            .take_while(|&s| {
+                cum += s;
+
+                if s < &min {
+                    min = *s;
+                }
+
+                if s > &max {
+                    max = *s;
+                }
+
+                cum < target_value
+            })
+            .count();
+
+        if cum == target_value {
+            return max + min;
+        }
+        skip = (skip + 1) % nums.len();
+    }
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
