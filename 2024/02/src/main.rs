e26620a1cf2781493ba3eb7fa2b2e71aa3b31ce9
Jack <40802798+Jack073@users.noreply.github.com>
2026-05-27 15:13:10 +0100

    add 2024 day 2 (2)

--- 2024/02/src/main.rs
+++ 2024/02/src/main.rs
@@ -23,6 +23,60 @@ fn part_one() -> usize {
         .count()
 }
 
+fn part_two() -> usize {
+    BufReader::new(File::open("input.txt").unwrap())
+        .lines()
+        .filter_map(|line| match line {
+            Ok(l) => Some(
+                l.split_ascii_whitespace()
+                    .map(|n| n.parse().unwrap())
+                    .collect::<Vec<isize>>(),
+            ),
+            Err(_) => None,
+        })
+        .filter(|row| {
+            let deltas = row.windows(2).map(|w| w[0] - w[1]).collect::<Vec<isize>>();
+            if deltas.windows(2).all(|w| w[0].signum() == w[1].signum())
+                && deltas.iter().all(|d| match d {
+                    -3 | -2 | -1 | 1 | 2 | 3 => true,
+                    _ => false,
+                })
+            {
+                return true;
+            }
+            let mut clone = Vec::with_capacity(row.len() - 1);
+            clone.resize(row.len() - 1, 0);
+            let mut index;
+            for i in 0..row.len() {
+                index = 0;
+                for j in 0..row.len() {
+                    if i == j {
+                        continue;
+                    }
+
+                    clone[index] = row[j];
+                    index += 1;
+                }
+
+                let diffs = clone
+                    .windows(2)
+                    .map(|w| w[0] - w[1])
+                    .collect::<Vec<isize>>();
+
+                if diffs.windows(2).all(|w| w[0].signum() == w[1].signum())
+                    && diffs.iter().all(|d| match d {
+                        -3 | -2 | -1 | 1 | 2 | 3 => true,
+                        _ => false,
+                    })
+                {
+                    return true;
+                }
+            }
+            false
+        })
+        .count()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
