0c7f0c63f91398deec36fbd60e603ce5d306473d
Jack <40802798+Jack073@users.noreply.github.com>
2026-06-02 12:00:09 +0100

    add 2022 day 4 (2)

--- 2022/04/src/main.rs
+++ 2022/04/src/main.rs
@@ -5,6 +5,11 @@ fn contains(s1: usize, e1: usize, s2: usize, e2: usize) -> bool {
     s1 <= s2 && e1 >= e2 && s2 <= e1
 }
 
+fn overlaps(s1: usize, e1: usize, s2: usize, e2: usize) -> bool {
+    s1 <= s2 && s2 <= e1
+}
+
+
 fn part_one() -> usize {
     BufReader::new(File::open("input.txt").expect("input.txt error"))
         .lines()
@@ -18,6 +23,19 @@ fn part_one() -> usize {
         .count()
 }
 
+fn part_two() -> usize {
+    BufReader::new(File::open("input.txt").expect("input.txt error"))
+        .lines()
+        .map(|l| {
+            l.unwrap()
+                .split(|c| c == '-' || c == ',')
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .filter(|l| overlaps(l[0], l[1], l[2], l[3]) || overlaps(l[2], l[3], l[0], l[1]))
+        .count()
+}
+
 fn main() {
-    println!("{}", part_one());
+    println!("{}", part_two());
 }
